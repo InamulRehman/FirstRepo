5bb0e1229f029a40229b34866f2ac724193e8377
Inam ul Rehman <inamulrehman09@gmail.com>
2021-03-17 14:21:55 +0500

    program.cs of first app updated

--- first_app/Program.cs
+++ first_app/Program.cs
@@ -7,6 +7,7 @@ namespace first_app
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            console.WriteLine("Helo i am inamulrehman ia am updating this ")
         }
     }
 }
