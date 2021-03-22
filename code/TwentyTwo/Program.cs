49180e131d216321831957476c893f60420117d8
Inam ul Rehman <inamulrehman09@gmail.com>
2021-03-22 14:51:43 +0500

    adding

---
+++ code/TwentyTwo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TwentyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("What is your name ? ");
+           
+        }
+    }
+}
