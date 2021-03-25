9bf61f0c56523d8c4490c1d1032499232ed07fd3
Inam ul Rehman <inamulrehman09@gmail.com>
2021-03-25 11:15:32 +0500

    updated

---
+++ code/InputC#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
