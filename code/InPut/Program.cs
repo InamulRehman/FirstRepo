0ca440eaed363df57debea4a12b409d48d6209bf
Inam ul Rehman <inamulrehman09@gmail.com>
2021-04-08 17:40:35 +0500

    update

---
+++ code/InPut/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InPut
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstName;
+            Console.Write("Enter your First name");
+            myFirstName=Console.ReadLine();
+            
+            string myLastName;
+            Console.Write("Enter your Last Name");
+            myLastName=Console.ReadLine();
+
+            Console.WriteLine(myFirstName+ " " +myLastName);
+
+        }
+    }
+}
