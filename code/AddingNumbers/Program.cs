c55d0849a00c7f217d22ef3b4f1e55ef75ea3b5b
Inam ul Rehman <inamulrehman09@gmail.com>
2021-04-08 18:19:35 +0500

    updated

---
+++ code/AddingNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+           int x ;
+           int y ;
+           x=55;
+           y = x + 10;
+           Console.WriteLine(y);
+        }
+    }
+}
