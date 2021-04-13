5655bb9bdac19379d2cc731dff7e4a36ff350e63
Inam ul Rehman <inamulrehman09@gmail.com>
2021-04-13 18:12:14 +0500

    code updated

---
+++ code/CompilerVersion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+// #error version
+
+namespace CompilerVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+             Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ code/Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Variales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerNum = 5;
+            float fValue = 10.4f;
+            char character = 'e';
+            byte number = 7;
+            string name = "Inam";
+            bool binary = false;
+
+            Console.WriteLine(integerNum);
+            Console.WriteLine(fValue); 
+            Console.WriteLine(character);
+            Console.WriteLine(number);
+            Console.WriteLine(name);
+            Console.WriteLine(binary);
+
+
+        }
+    }
+}
