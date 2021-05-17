f0eb23f07ce7c1bc53ed6f2b13c8c888efc4f1d2
InamulRehman <inamulrehman09@gmail.com>
2021-05-17 20:34:43 +0500

    codeupdated

---
+++ code/Numbers/ExploringNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExploringNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // unsigned integer means positive whole number
+// including 0
+uint naturalNumber = 23;
+// integer means negative or positive whole number 
+// including 0
+int integerNumber = -23;
+// float means single-precision floating point
+// F suffix makes it a float literal               
+float realNumber = 2.3F; 
+// double means double-precision floating point
+// double literal
+double anotherRealNumber = 2.3; 
+Console.WriteLine(naturalNumber);
+Console.WriteLine(integerNumber);
+Console.WriteLine(realNumber);
+Console.WriteLine(anotherRealNumber);
+        }
+    }
+}
