f9c2c66047c55a6c662bfb52fa723ad19a6d127e
InamulRehman <inamulrehman09@gmail.com>
2021-06-10 18:42:02 +0500

    for each loop added

---
+++ code/Control Flow/Iteration Statements_Loops/ForEachLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Saqib Ali";
+            //using for loop
+            for (var i=0; i< name.Length; i++ ){
+
+                Console.WriteLine(name[i]);
+
+            }
+            // using for each loop 1
+            foreach(var character in name){
+
+                Console.WriteLine(character);
+            }
+            // using for each loop 2
+            var numbers = new int [] {2,5,7,4};
+            foreach(var number in numbers){
+                
+                Console.WriteLine(number);
+            }
+
+
+
+        }
+    }
+}
