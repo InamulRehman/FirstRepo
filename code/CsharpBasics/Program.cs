c946130eefd4f6eae07ddfeaa436985bd758170f
InamulRehman <inamulrehman09@gmail.com>
2021-06-10 16:57:11 +0500

    c sharp basics

---
+++ code/CsharpBasics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CsharpBasics
+{
+    public class Person{
+
+        public int age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment (number);
+            Console.WriteLine(number);
+            var person = new Person() {age = 20};
+
+            MakeOld(person);
+            Console.WriteLine(person.age);
+
+        }
+        public static void Increment (int number)
+            {
+                number += 10;
+
+             }
+        public static void MakeOld(Person person)
+            {
+                person.age += 10;
+            }
+    }
+}
