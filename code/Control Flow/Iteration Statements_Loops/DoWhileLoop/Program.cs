0ca4b24063ae991c23a3a0b78558f2f30d1c392c
InamulRehman <inamulrehman09@gmail.com>
2021-06-14 12:11:06 +0500

    random class and dowhile loop added

---
+++ code/Control Flow/Iteration Statements_Loops/DoWhileLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i=0;
+            do {
+
+                if (i%2 ==0)
+                    Console.WriteLine(i);              // in this loop will excute once either condition is true/false
+                    i++;                            // also this loop terminated by semicolon unlike while loop
+                    
+            }while(i<=10);
+        }
+    }
+}
---
+++ code/Control Flow/Iteration Statements_Loops/RandomClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var random = new Random();                                   // for detail see description
+            for(var i=0; i<10; i++)
+                Console.WriteLine(random.Next(19985, 19987));
+
+            Console.WriteLine((int)'A');                  // returns the ascii/unicode equivalent of specified character, for more see description
+            
+
+         
+            var ran = new Random();
+           for (var j=0; j<10; j++)
+                Console.Write((char)ran.Next(97 , 110)) ;      // 97-122 range for small letters
+            
+              //Console.WriteLine( "inam" + 'a');
+            
+            const int passworsLength = 10;
+            var rand = new Random();
+            var buffer = new char[passworsLength];  //10
+            for (var y=0; y<passworsLength; y++)
+               // Console.Write((char) ('a'+ random.Next(0, 26)));
+                buffer[y]=((char) ('a'+ rand.Next(0, 26)));
+                //Console.WriteLine();
+            var password = new string(buffer);
+            Console.WriteLine(password);
+
+        
+                
+
+           
+        }
+    }
+}
