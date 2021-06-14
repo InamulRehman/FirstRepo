6dd4230b46115788e579d8d193c57f7048b3ec97
InamulRehman <inamulrehman09@gmail.com>
2021-06-14 13:11:51 +0500

    conditional_statements  multiple if _ conditional structure added

---
+++ code/Control Flow/Conditional Statements_if,,,/MultipleIf/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MultipleIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+            if (hour > 0 && hour < 12)             // multiple if else structure
+
+                Console.WriteLine("It's morning");
+                                                         // we will use curly braces {} when there are more than one statements
+            else if (hour >= 12 && hour < 18 )
+
+                Console.WriteLine("It's afternoon");
+
+            else
+
+                Console.WriteLine("It's evening");
+
+            bool isGoldCustomer = true;//not understanding about working
+            
+            float price;
+            if (isGoldCustomer)                       // simple if else structure
+                price = 19.95f;
+            else
+                price = 29.95f;
+            Console.WriteLine(price);
+            
+
+            float pri = (isGoldCustomer) ? 19.95f : 29.95f;      // conditional operator works just like simple if else structure
+            Console.WriteLine(pri);
+
+                
+
+
+            
+        }
+    }
+}
