829180addcae42c529a2bc1ea8a17ba01ca19e3b
InamulRehman <inamulrehman09@gmail.com>
2021-06-25 02:05:43 +0500

    working with files addded

---
+++ code/7. working_with_files/WorkingWithFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path1 = @"F:\a.png";
+           /* var path = @"E:\a.png";
+            File.Copy(@"E:\b.png" , @"F:\b.png", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                
+            }
+            var content = File.ReadAllText(path);*/
+
+
+            var fileInfo = new FileInfo(path1);
+            fileInfo.CopyTo("...") ;
+            fileInfo.Delete();
+            if(fileInfo.Exists)
+            
+                
+            
+        }
+    }
+}
