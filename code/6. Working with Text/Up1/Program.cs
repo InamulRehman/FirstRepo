0038deff2531865adde45498eab58b60b92ac98d
InamulRehman <inamulrehman09@gmail.com>
2021-06-24 20:15:29 +0500

    UsingProcedures and Up1 added

---
+++ code/6. Working with Text/Up1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Up1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first practice of using logic into another method , separately from console(procedural programing)
+            Console.WriteLine("What is your name ");
+            var name = Console.ReadLine();
+
+            var reversed = ReverseName(name);    
+            Console.WriteLine("Reversed name is: " + reversed);
+        
+             static string ReverseName(string name)
+            {
+
+                var arry = new char[name.Length];
+                for (var i = name.Length; i>0; i-- )
+                    arry[name.Length - 1] = name[i -1]; // not understanding the logic
+
+            // var reversed = new string (arry);
+
+                return new string(arry);
+                
+            }
+        }
+
+
+    }
+}
---
+++ code/6. Working with Text/UsingProcedures/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingProcedures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var numbers = new List<int>();
+
+        
+           while(true)
+            {
+                Console.Write("Enter a number ( or 'quit' to exit): ");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+                numbers.Add(Convert.ToInt32(input));  
+            }
+
+    
+            Console.WriteLine("Unique Numbers: ");
+            
+            foreach(var number in GetUniqueNumbers(numbers))
+                Console.WriteLine(number);
+
+         static List<int> GetUniqueNumbers(List<int>numbers)
+         {
+            var uniques = new List<int>();
+            foreach(var number in numbers)
+            {
+                if(!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+            return uniques;
+          } 
+
+
+    
+
+        }
+        
+    }
+}
