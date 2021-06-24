578b45c70e99a04779d13d73f208aab4036d87f5
InamulRehman <inamulrehman09@gmail.com>
2021-06-24 14:50:02 +0500

    updated working text with StringBuilder

---
+++ code/6. Working with Text/StringManipulate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace StringManipulate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-',10);
+            builder.AppendLine();   ///like endl
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-',10);
+            builder.Replace('-','*');
+            builder.Remove(0,10);
+            builder.Insert(0, new string('-', 10));
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First character: {0}", builder[0]);
+        }
+    }
+}
