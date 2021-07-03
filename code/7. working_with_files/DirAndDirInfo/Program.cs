8d93530c3b97e0ce3a40345bf0bfbbfd999c2b3f
InamulRehman <inamulrehman09@gmail.com>
2021-07-03 11:47:18 +0500

    file/dir, fileinfo/dirinfo and path added

---
+++ code/7. working_with_files/DirAndDirInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace DirAndDirInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Directory.CreateDirectory(@"F:\dir\dir2\dir3\newone");
+            // var files = Directory.GetFiles(@"F:\", "* .pdf" ,SearchOption.AllDirectories);
+            // foreach(var file in files ) 
+            // {          //not working why?
+
+            //     Console.WriteLine(file);
+            // }
+            
+            // var dirs = Directory.GetDirectories(@"C:\Users\Inam UL rehman\Desktop\vp-csharp\code\" ,"*.*" SearchOption.AllDirectories);
+            // foreach (var dir in dirs)
+            // {
+            //         Console.WriteLine(dir); 
+        
+            // }
+
+            var diroctoryinfo = new DirectoryInfo(@"\Users\Inam UL rehman\Desktop\csharp-01\code")  ;
+            var diroctoriesinfo = diroctoryinfo.GetDirectories("*.*", SearchOption.AllDirectories)  ;
+            foreach(var diroctory in diroctoriesinfo)
+                {
+                    Console.WriteLine(diroctory);
+                }    
+        }
+        
+    }
+}
---
+++ code/7. working_with_files/Path1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Path1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Inam UL rehman\Desktop\csharp-01\code\HeloWorld.snl";
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+            Console.WriteLine("Extensions: {0}", extension);
+            Console.WriteLine("Extensions: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Diroctory name: " + Path.GetDirectoryName(path));
+    
+        }
+    }
+}
