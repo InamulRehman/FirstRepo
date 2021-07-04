a7ce135fd2a672c7b0144e0fbdddfabc79e35e15
InamulRehman <inamulrehman09@gmail.com>
2021-07-04 22:49:25 +0500

    Conditionals\mltipleif , Conditionals\Switch updated

--- code/3. Control Flow/Conditional Statements_if,,,/MultipleIf/Program.cs
+++ code/3. Control Flow/Conditional Statements_if,,,/MultipleIf/Program.cs
@@ -8,29 +8,37 @@ namespace MultipleIf
         {
             int hour = 10;
             if (hour > 0 && hour < 12)             // multiple if else structure
-
+            {
                 Console.WriteLine("It's morning");
-                                                         // we will use curly braces {} when there are more than one statements
+            }                                             
             else if (hour >= 12 && hour < 18 )
-
+            {
                 Console.WriteLine("It's afternoon");
-
+            }
             else
-
+            {
                 Console.WriteLine("It's evening");
+            }    
 
-            bool isGoldCustomer = true;//not understanding about working
+           bool isGoldCustomer = true;              //not understanding about working
             
             float price;
-            if (isGoldCustomer)                       // simple if else structure
-                price = 19.95f;
+            
+            if (isGoldCustomer)  
+             {
+                  price = 19.95f;
+             }                              // simple if else structure
+               
             else
-                price = 29.95f;
+            {
+                  price = 29.95f;
+            }
+               
             Console.WriteLine(price);
             
 
-            float pri = (isGoldCustomer) ? 19.95f : 29.95f;      // conditional operator works just like simple if else structure
-            Console.WriteLine(pri);
+            //  float pri = (isGoldCustomer) ? 19.95f : 29.95f;      // conditional operator works just like simple if else structure
+            //  Console.WriteLine(pri);
 
                 
 
